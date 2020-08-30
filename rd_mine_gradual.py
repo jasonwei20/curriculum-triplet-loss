069ae1be1c8966daab1d5b007777a0dd45f484f7
Jason Wei <jason.weng.wei@gmail.com>
2020-08-30 14:26:43 -0400

    first working commit

---
+++ rd_mine_gradual.py
@@ -0,0 +1,16 @@
+from utils import common, configuration, triplet_cl_methods
+
+if __name__ == "__main__":
+
+    cfg_json_list = [
+        "config/prelim/fewrel_nc5_rd_c.json",
+    ]
+
+    for cfg_json in cfg_json_list:
+
+        cfg = configuration.triplet_ap_cl_gradual_config.from_json(cfg_json); print(f"config from {cfg_json}")
+
+        for seed_num in range(cfg.num_seeds):
+
+            common.set_random_seed(seed_num)
+            triplet_cl_methods.train_eval_cl_gradual_model(cfg, seed_num)
---
+++ utils/configuration.py
@@ -0,0 +1,48 @@
+import json
+from typing import NamedTuple
+
+class triplet_ap_cl_gradual_config(NamedTuple):
+
+    exp_id: str = None
+    num_seeds: int = 0
+
+    #dataset params
+    train_path: str = None
+    test_path: str = None
+    num_output_classes: int = None
+
+    # model params
+    max_length: int = 64
+    encoding_size: int = 768 # bert embedding size
+    embedding_size: int = 30 # output embedding size
+    triplet_margin: float = 0.4
+    learning_rate: float = 2e-5
+
+    # training params
+    train_nc: int = None
+    val_subset: int = 1000
+    total_updates: int = 10000
+    eval_interval: int = 100
+
+    # augmentation params
+    n_aug: int = None
+    n_orig: int = None
+    aug_type: str = None 
+    second_stage_alpha: int = None
+    third_stage_alpha: int = None
+    fourth_stage_alpha: int = None
+    fifth_stage_alpha: int = None
+    sixth_stage_alpha: int = None
+    curriculum_type: str = None # "curriculum", "random", "anti", "no-aug"
+
+    # cl params
+    first_stage_updates: int = None
+    second_stage_updates: int = None
+    third_stage_updates: int = None
+    fourth_stage_updates: int = None
+    fifth_stage_updates: int = None
+    hard_negative_mining: str = None
+
+    @classmethod
+    def from_json(cls, file_path):
+        return cls(**json.load(open(file_path, 'r')))
